bba357a4b6aa906580cd8746fdcae9f9301e235d
xiayefeng <xiayu_12@yeah.net>
2019-10-28 00:33:49 +0800

    Update demo1.js

--- src/demo1.js
+++ src/demo1.js
@@ -94,7 +94,7 @@ class App extends Component{
       );
   }
 } */
-
+/*
 class Child extends Component{
   render() {
           return (
@@ -129,6 +129,41 @@ class App extends Component{
                             }}>set</button>
                    </div>
           );
+  }
+}*/
+
+class App extends Component{
+  state = {
+    num: 1
+  }
+  render() {
+    return (
+      <div>
+        sss
+      </div>
+    );
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.num)
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      console.log('timeout')
+    })
+    new Promise((resovle) => {
+      console.log('before')
+      resovle('then')
+      console.log('after')
+    }).then(res => {
+      console.log(res)
+    })
+    let {num} = this.state
+    this.setState({
+      num: ++num
+    }, () => {
+      console.log('after setState')
+    })
+    console.log('---')
   }
 }
 
